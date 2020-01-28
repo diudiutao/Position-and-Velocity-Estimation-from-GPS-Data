981756edc407861d0f91e6a438092dc7ae6ac00b
stalin170296 <stalin170296@hotmail.com>
2020-01-28 11:37:57 -0500

    Aplicación para estimar la posición y la velocidad en datos de posicionamiento global

--- GpsEstimation.py
+++ GpsEstimation.py
@@ -65,7 +65,7 @@ def GEOTOUTM():
 
 def GRAFICAS():
     plt.plot(grapmedx, grapmedy, '.-', label='Mediciones')
-    plt.plot(graprealx, graprealy, '-', label='Ruta Real')
+    plt.plot(Pos_Real_X, Pos_Real_Y, '-', label='Ruta Real')
     plt.plot(px_est, py_est, '-', label='Estimacion')
     plt.xlabel('X (m)')  # nombra el eje x
     plt.ylabel('Y (m)')  # nombra el eje y
@@ -96,19 +96,18 @@ def kf_update(X, P, Y, H, R):
     P = np.dot(P, (I - np.dot(K, H)))
     return X, P
 
-
-
 GEOTOUTM()
-N_iter = 100
-dt = 1.0/N_iter
+
+Niter = 2
+time = 1 #tiempo de muestreo
+dt = (1.0 / Niter)*time
 # aceleracion en x e y
 ax = 0.0
 ay = 0.0
-m = (Pos_Real_Y[1] - Pos_Real_Y[0]) / (Pos_Real_X[1] - Pos_Real_X[0])  # calculo de la pendiente
-ang = math.atan(m)  # calculo del angulo (respuesta en radianes)
+#m = (Pos_Real_Y[1] - Pos_Real_Y[0]) / (Pos_Real_X[1] - Pos_Real_X[0])  # calculo de la pendiente
+#ang = math.atan((m))  # calculo del angulo (respuesta en radianes)
 px = Pos_Real_X[0]  # posicion inicial x
 py = Pos_Real_Y[0]  # posicion inicial y
-
 # espacio de estados
 A = np.array([[1.0, 0.0, dt, 0.0], [0.0, 1.0, 0.0, dt], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
 X = np.array([[Pos_Real_X[0]], [Pos_Real_Y[0]], [0.0], [0.0]])  # condiciones iniciales
@@ -118,7 +117,7 @@ W = 0  # ruido en la medicion
 V = 0 #ruido en el proceso
 #P = 0.1   # covarianza
 #Q = np.diag((0.0071, 0.0081, 0.0003, 0.0326)) # covarianza de ruido en el proceso
-Q = 0.00001
+Q = 0.0001
 P = np.diag((0.1, 0.1, 0.1, 0.1))
 #Q = np.dot(B, B.T)
 Z = 0  # ruido en la medicion
@@ -127,27 +126,44 @@ Z = 0  # ruido en la medicion
 H = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
 I = np.identity(4)
 
-
+aux = 0
+m = (Pos_Real_Y[aux+1] - Pos_Real_Y[aux]) / (Pos_Real_X[aux+1] - Pos_Real_X[aux])  # calculo de la pendiente
+ang = math.atan((m))  # calculo del angulo (respuesta en radianes)
 # calculos luego de las mediciones
+print((len(lonMed)/len(lonReal)))
+print(len(lonReal))
 for i in np.arange(1, len(lonMed)):
     Exactitud = datosGPS['accuracy'].ix[i]
     R = np.diag(((Exactitud)**2,(Exactitud)**2,(Exactitud)**2,(Exactitud)**2))
-    if Pos_Real_Y[1] < Pos_Real_Y[0]:
-
-        vx = -V_Real[i] * math.cos(ang) # calculo la velocidad en x
-        vy = -V_Real[i] * math.sin(ang) # calculo la velocidad en y
-
-        ax = -(V_Real[i] - V_Real[i - 1]) * math.cos(ang) * dt #calculo la aceleracion en x
-        ay = -(V_Real[i] - V_Real[i - 1]) * math.sin(ang) * dt # calculo la aceleracion en y
-    else:
-        vx = V_Real[i] * math.cos(ang)  # calculo la velocidad en x
-        vy = V_Real[i] * math.sin(ang)  # calculo la velocidad en y
-
-        ax = (V_Real[i] - V_Real[i - 1]) * math.cos(ang) * dt  # calculo la aceleracion en x
-        ay = (V_Real[i] - V_Real[i - 1]) * math.sin(ang) * dt  # calculo la aceleracion en y
+    if (i % ((len(lonMed)/len(lonReal)))) == 0:
+        aux = aux + 1
+        if aux >= (len(lonReal)-1):
+            aux = aux-1
+        m = (Pos_Real_Y[aux+1] - Pos_Real_Y[aux]) / (Pos_Real_X[aux+1] - Pos_Real_X[aux])  # calculo de la pendiente
+        ang = math.atan((m))  # calculo del angulo (respuesta en radianes)
+    if Pos_Real_X[aux+1] > Pos_Real_X[aux] and Pos_Real_Y[aux+1] > Pos_Real_Y[aux]: #primer cuadrante
+        vx = V_Real[i] * math.cos(abs(ang))  # calculo la velocidad en x
+        vy = V_Real[i] * math.sin(abs(ang))  # calculo la velocidad en y
+        ax = (V_Real[i] - V_Real[i - 1]) * math.cos(abs(ang)) / time  # calculo la aceleracion en x
+        ay = (V_Real[i] - V_Real[i - 1]) * math.sin(abs(ang)) / time  # calculo la aceleracion en y
+    elif Pos_Real_X[aux+1] < Pos_Real_X[aux] and Pos_Real_Y[aux+1] > Pos_Real_Y[aux]: #segundo cuadrante
+        vx = V_Real[i] * math.cos(math.pi-abs(ang))  # calculo la velocidad en x
+        vy = V_Real[i] * math.sin(math.pi-abs(ang))  # calculo la velocidad en y
+        ax = (V_Real[i] - V_Real[i - 1]) * math.cos(math.pi-abs(ang)) / time  # calculo la aceleracion en x
+        ay = (V_Real[i] - V_Real[i - 1]) * math.sin(math.pi-abs(ang)) / time  # calculo la aceleracion en y
+    elif Pos_Real_X[aux+1] < Pos_Real_X[aux] and Pos_Real_Y[aux+1] < Pos_Real_Y[aux]:  # tercer cuadrante
+        vx = V_Real[i] * math.cos(abs(ang)-math.pi)  # calculo la velocidad en x
+        vy = V_Real[i] * math.sin(abs(ang)-math.pi)  # calculo la velocidad en y
+        ax = (V_Real[i] - V_Real[i - 1]) * math.cos(abs(ang)-math.pi) / time  # calculo la aceleracion en x
+        ay = (V_Real[i] - V_Real[i - 1]) * math.sin(abs(ang)-math.pi) / time  # calculo la aceleracion en y
+    elif Pos_Real_X[aux+1] > Pos_Real_X[aux] and Pos_Real_Y[aux+1] < Pos_Real_Y[aux]:  # tercer cuadrante
+        vx = V_Real[i] * math.cos((2*math.pi)-abs(ang))  # calculo la velocidad en x
+        vy = V_Real[i] * math.sin((2*math.pi)-abs(ang))  # calculo la velocidad en y
+        ax = (V_Real[i] - V_Real[i - 1]) * math.cos((2*math.pi)-abs(ang)) / time  # calculo la aceleracion en x
+        ay = (V_Real[i] - V_Real[i - 1]) * math.sin((2*math.pi)-abs(ang)) / time  # calculo la aceleracion en y
 
     d_med = BEARING(latMed[i - 1], lonMed[i - 1], latMed[i], lonMed[i])
-    v_med = d_med / 1
+    v_med = d_med / time
 
     mx = grapmedx[i]    #posicion medida
     my = grapmedy[i]    #posicion medida
@@ -156,7 +172,7 @@ for i in np.arange(1, len(lonMed)):
     U = np.array([[ax], [ay]])
 
     # aplicando el filtro de kalman
-    for j in np.arange(0, N_iter):
+    for j in np.arange(0, Niter):
         Grap_V_real.append(V_Real[i])
         V_Medida.append(v_med)
 
